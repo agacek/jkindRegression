88ad3c06f6c5ac4622d93aa812fe0f8186ed7462
Paul Martin <p.r.martin.sw@gmail.com>
2015-09-16 09:08:21 -0700

    Checkpoint: Everything runs, but getting strange errors validating the pre.lus

--- newtest/results.py
+++ newtest/results.py
@@ -7,7 +7,7 @@ class JKindResult( object ):
         self.d = dict()
         self.fname = filename
         self.args = argString
-        self.failures = list()
+        self.failLog = list()
 
     def __setitem__( self, key, val ):
         self.d[key] = val
@@ -17,7 +17,7 @@ class JKindResult( object ):
 
     def __eq__( self, other ):
         self.other = other
-        self.failures = list()
+        self.failLog = list()
 
         if isinstance( other, self.__class__ ):
             a = sorted( self.d.keys() )
@@ -27,25 +27,32 @@ class JKindResult( object ):
                 self._logFailure( err )
 
             for key in a:
-                if( ( self.d[key] != 'unknown' ) and ( other.d[key] != 'unknown' ) ):
-                    if( self.d[key] != other.d[key] ):
-                        err = 'Key/Value mismatch: {}:{} {}:{}'.format( key,
-                                                                        self.d[key],
-                                                                        key,
-                                                                        other.d[key] )
-                        self._logFailure( err )
+                try:
+                    if( ( self.d[key] != 'unknown' ) and ( other.d[key] != 'unknown' ) ):
+                        if( self.d[key] != other.d[key] ):
+                            err = 'Key/Value mismatch: {}:{} {}:{}'.format( key,
+                                                                            self.d[key],
+                                                                            key,
+                                                                            other.d[key] )
+                            self._logFailure( err )
+                except KeyError as e:
+                    err = 'Missing field {}'.format( str( e ) )
+                    self._logFailure( err )
         else:
             raise AssertionError( 'Invalid class type for equality' )
 
-        return ( len( self.failures ) == 0 )
+        return ( len( self.failLog ) == 0 )
 
     def _logFailure( self, err ):
         s = '{}: <{}> <{}>\n  '.format( self.filename(), self.arguments(), self.other.arguments() )
         s += err
-        self.failures.append( s )
+        self.failLog.append( s )
 
     def __ne__( self, other ):
         return not self.__eq__( other )
+
+    def failures( self ):
+        return self.failLog
 
     def filename( self ):
         return self.fname
--- newtest/testcase.py
+++ newtest/testcase.py
@@ -9,9 +9,6 @@ class MyTestCase( unittest.TestCase ):
     def __init__( self, methodName = 'runTest' ):
         unittest.TestCase.__init__( self, methodName = methodName )
 
-    def test1( self ):
-        pass
-
     def setUp( self ):
         self.file = TestConfig().popFile()
         self.args = TestConfig().getArguments()
@@ -22,3 +19,23 @@ class MyTestCase( unittest.TestCase ):
 
     def tearDown( self ):
         pass
+
+    def test1( self ):
+        resultsList = self.results.copy()
+        controlList = resultsList.pop()
+
+        for each in resultsList:
+            ok = ( controlList == each )
+            if( ok == False ):
+                for jkr in controlList:
+                    # print( type( jkr.failures() ) )
+                    for line in ( jkr.failures() ):
+                        print( line )
+
+
+            # msg = 'Test {} Args <{}> <{}>'.format()
+            self.assertTrue( ok, 'test equality' )
+
+
+
+
